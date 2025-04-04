77b9be892bf1da37b8aef0b3582cf245203d3ea8
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-03 21:47:46 -0300

    feat(app/common): altera funcao padrao p/ geracao dos id's nas entidades
    passa a utilizar funcao que gera um UUID v7

--- src/app/_common/Model.ts
+++ src/app/_common/Model.ts
@@ -1,8 +1,10 @@
 import { AbstractModel, AbstractModelProps } from "ts-arch-kit/dist/core/models";
 
+import { UUID } from "@/shared/helpers";
+
 export abstract class Model<T extends AbstractModelProps> extends AbstractModel<T> {
     protected constructor(props: T) {
-        super(props, !props.id);
+        super(props, !props.id, UUID.v7);
     }
 
     getId(): string {

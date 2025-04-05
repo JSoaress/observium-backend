7ac0791c332f5f6b812a2090ba54a320908b439e
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-05 12:19:32 -0300

    feat(shared): add tratamento para env vars utilizando a lib env-var

---
+++ src/shared/config/environment.ts
@@ -0,0 +1,20 @@
+import envVar from "env-var";
+
+export const env = {
+    nodeEnv: envVar.get("NODE_ENV").default("development").asString(),
+    platformName: envVar.get("PLATFORM").default("Observium").asString(),
+    authTokenSecret: envVar.get("JWT_TOKEN_SECRET").required().asString(),
+    authTokenExpiresIn: envVar.get("JWT_TOKEN_EXPIRES_IN").default("1d").asString(),
+    databaseProvider: envVar.get("DATABASE_PROVIDER").required().asString(),
+    databaseHost: envVar.get("DATABASE_HOST").required().asString(),
+    databasePort: envVar.get("DATABASE_PORT").required().asInt(),
+    databaseName: envVar.get("DATABASE_DB").required().asString(),
+    databaseUser: envVar.get("DATABASE_USER").required().asString(),
+    databasePassword: envVar.get("DATABASE_PASSWORD").required().asString(),
+    mailProvider: envVar.get("MAIL_PROVIDER").required().asString(),
+    mailHost: envVar.get("MAIL_HOST").required().asString(),
+    mailPort: envVar.get("MAIL_PORT").required().asInt(),
+    mailUser: envVar.get("MAIL_USER").required().asEmailString(),
+    mailPass: envVar.get("MAIL_PASS").required().asString(),
+    webUrl: envVar.get("WEB_URL").required().asUrlString(),
+};

b5e43bc0dc737732d00ffc2bf98fdb2990666d9d
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-05 12:20:09 -0300

    feat(app/users): ajusta envio de email de ativação após cadastro do usuário na plataforma

--- src/app/users/application/use-cases/users/create-user/use-case.ts
+++ src/app/users/application/use-cases/users/create-user/use-case.ts
@@ -1,3 +1,4 @@
+import { resolve } from "node:path";
 import { left, right } from "ts-arch-kit/dist/core/helpers";
 import { UnitOfWork } from "ts-arch-kit/dist/database";
 
@@ -5,6 +6,7 @@ import { UseCase } from "@/app/_common";
 import { EmailTakenError } from "@/app/_common/errors";
 import { User, UserToken } from "@/app/users/domain/models/user";
 import { IMail } from "@/infra/providers/mail";
+import { env } from "@/shared/config/environment";
 
 import { IUserRepository, IUserTokenRepository } from "../../../repos";
 import { CreateUserUseCaseGateway, CreateUserUseCaseInput, CreateUserUseCaseOutput } from "./types";
@@ -35,10 +37,12 @@ export class CreateUserUseCase extends UseCase<CreateUserUseCaseInput, CreateUse
             const userTokenOrError = UserToken.createActivation({ user: newUser });
             if (userTokenOrError.isLeft()) return left(userTokenOrError.value);
             const newUserToken = await this.userTokenRepository.save(userTokenOrError.value);
-            const templatePath = "";
+            const templatePath = resolve(__dirname, "..", "..", "views", "emails", "activate-account.hbs");
             const variables = {
+                platform: env.platformName,
                 name: newUser.name,
-                link: `/activation-account/${newUserToken.token}`,
+                activationLink: `${env.webUrl}/activation-account?token=${newUserToken.token}`,
+                year: new Date().getFullYear(),
             };
             await this.mailProvider.sendMail(newUser.email, "Ativação conta Observium", templatePath, variables);
             return right(newUser);

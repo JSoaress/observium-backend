a85e30f708368bc041d19d6245846f73a8abb847
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-04 23:09:26 -0300

    feat(infra/db): add arquivos de configuracoes da lib knex

---
+++ src/infra/database/knex/knexfile.ts
@@ -0,0 +1,59 @@
+import type { Knex } from "knex";
+
+const config: { [key: string]: Knex.Config } = {
+    test: {
+        client: "sqlite3",
+        connection: {
+            filename: "./dev.sqlite3",
+        },
+    },
+    development: {
+        client: "pg",
+        connection: {
+            database: "observium",
+            user: "postgres",
+            password: "postgres",
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            extension: "ts",
+            stub: "migration.stub",
+            tableName: "knex_migrations",
+        },
+    },
+    staging: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password",
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+        },
+    },
+    production: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password",
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+        },
+    },
+};
+
+export default config;

58158cb5dbc4815a77cf3ed286ad3a39295e3ce8
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-06 23:50:11 -0300

    feat(app/auth): permite ao AuthenticationDecorator verificar token do usuario e apikey

--- src/app/auth/application/use-cases/authentication-decorator/AuthenticationDecorator.ts
+++ src/app/auth/application/use-cases/authentication-decorator/AuthenticationDecorator.ts
@@ -3,6 +3,7 @@ import { BasicError } from "ts-arch-kit/dist/core/errors";
 import { Either, left } from "ts-arch-kit/dist/core/helpers";
 
 import { UseCase } from "@/app/_common";
+import { CheckAPIKeyUseCase } from "@/app/users/application/use-cases/api-keys/check-api-key";
 import { IJwt } from "@/infra/adapters/jwt";
 import { IRepositoryFactory } from "@/infra/database";
 
@@ -25,11 +26,19 @@ export class AuthenticationDecorator<TInput, TOutput extends Either<BasicError, 
     }
 
     protected async impl({ requestUserToken, ...input }: AuthenticationDecoratorInput<TInput>): Promise<TOutput> {
+        const [prefix, token] = requestUserToken.split(" ");
+        if (prefix?.toLowerCase() === "apikey") {
+            const authenticateUseCase = new CheckAPIKeyUseCase({ repositoryFactory: this.repositoryFactory });
+            const authenticateOrError = await authenticateUseCase.execute({ key: token });
+            if (authenticateOrError.isLeft()) return left(authenticateOrError.value) as TOutput;
+            const requestUser = authenticateOrError.value;
+            return this.useCase.execute({ ...input, requestUser } as TInput);
+        }
         const authenticateUseCase = new CheckAuthenticatedUserUseCase({
             repositoryFactory: this.repositoryFactory,
             jwtAdapter: this.jwtAdapter,
         });
-        const authenticateOrError = await authenticateUseCase.execute({ requestUserToken });
+        const authenticateOrError = await authenticateUseCase.execute({ requestUserToken: token });
         if (authenticateOrError.isLeft()) return left(authenticateOrError.value) as TOutput;
         const requestUser = authenticateOrError.value;
         return this.useCase.execute({ ...input, requestUser } as TInput);

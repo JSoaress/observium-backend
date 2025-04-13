3ca9e7f593b6cb1ff7dfb6512dfe2229480343c4
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-13 12:05:33 -0300

    feat(infra/presenters): add classe LogSimplifiedJsonPresenter

--- src/infra/presenters/json/index.ts
+++ src/infra/presenters/json/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-classes-per-file */
 import { IPresenter } from "ts-arch-kit/dist/core/helpers";
 
-import { Log, LogDTO } from "@/app/logs/domain/models/log";
+import { Log, LogDTO, LogSimplifiedDTO } from "@/app/logs/domain/models/log";
 import { Project, ProjectDTO } from "@/app/logs/domain/models/project";
 import { APIKey, APIKeyDTO } from "@/app/users/domain/models/api-key";
 import { User, UserDTO } from "@/app/users/domain/models/user";
@@ -59,6 +59,7 @@ export class LogJsonPresenter implements IPresenter<Log, LogJson> {
             projectId: input.projectId,
             path: input.path,
             method: input.method,
+            externalId: input.externalId,
             statusCode: input.statusCode,
             statusText: input.statusText,
             level: input.level,
@@ -66,6 +67,26 @@ export class LogJsonPresenter implements IPresenter<Log, LogJson> {
             context: input.context,
             response: input.response,
             error: input.error,
+            createdAt: input.createdAt,
+        };
+    }
+}
+
+export type LogSimplifiedJson = LogSimplifiedDTO;
+
+export class LogSimplifiedJsonPresenter implements IPresenter<LogSimplifiedDTO, LogSimplifiedJson> {
+    present(input: LogSimplifiedDTO): LogSimplifiedJson {
+        return {
+            id: input.id,
+            type: input.type,
+            projectId: input.projectId,
+            path: input.path,
+            method: input.method,
+            externalId: input.externalId,
+            statusCode: input.statusCode,
+            statusText: input.statusText,
+            level: input.level,
+            duration: input.duration,
             createdAt: input.createdAt,
         };
     }

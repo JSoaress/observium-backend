e88bcd9e3b49662788d07c3090b7c45897f3a44b
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-24 22:49:22 -0300

    chore: carrega arquivo .env.test.local ao iniciar os testes

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import path from "node:path";
 import { defineConfig } from "vitest/config";
+
+dotenv.config({ path: "./.env.test.local" });
 
 export default defineConfig({
     resolve: {

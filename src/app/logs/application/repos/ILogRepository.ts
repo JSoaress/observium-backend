5abfee97a5ebf42904b5503c1d0f747de7d4346c
João Vitor <joao_vitorsgs@hotmail.com>
2025-04-25 19:11:01 -0300

    chore(app/logs): ajusta tipagem do contrato ILogRepository

--- src/app/logs/application/repos/ILogRepository.ts
+++ src/app/logs/application/repos/ILogRepository.ts
@@ -2,9 +2,9 @@ import { QueryOptions } from "ts-arch-kit/dist/database";
 
 import { IRepository } from "@/infra/database";
 
-import { LogDTO, Log, LogSimplifiedDTO, TotalDailyLogs, LogType } from "../../domain/models/log";
+import { LogDTO, Log, LogSimplifiedDTO, TotalDailyLogs } from "../../domain/models/log";
 
-export type LogRepositoryWhere = { type: LogType; duration: number } & LogDTO;
+export type LogRepositoryWhere = LogDTO;
 
 export interface ILogRepository extends IRepository<Log, LogRepositoryWhere> {
     findSimplified(queryOptions?: QueryOptions): Promise<LogSimplifiedDTO[]>;

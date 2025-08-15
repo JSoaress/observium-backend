866c740c8925cfdfaf840071cc0611c7161a239d
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-14 22:15:17 -0300

    feat(app/common): flexibiliza geracao de id na classe Model

--- src/app/_common/Model.ts
+++ src/app/_common/Model.ts
@@ -1,10 +1,10 @@
-import { AbstractModel, AbstractModelProps } from "ts-arch-kit/dist/core/models";
+import { AbstractModel, AbstractModelProps, PrimaryKey } from "ts-arch-kit/dist/core/models";
 
 import { UUID } from "@/shared/helpers";
 
 export abstract class Model<T extends AbstractModelProps> extends AbstractModel<T> {
-    protected constructor(props: T) {
-        super(props, !props.id, UUID.v7);
+    protected constructor(props: T, idGenerator: () => PrimaryKey = UUID.v7) {
+        super(props, !props.id, idGenerator);
     }
 
     getId(): string {

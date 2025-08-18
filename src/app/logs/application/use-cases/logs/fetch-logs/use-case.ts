0747320cdabcc02a8e31c3ff66ae4013ff634ad9
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-18 19:49:13 -0300

    feat(app/logs): valida se usuario pertence a workspace no use case FetchLogs

--- src/app/logs/application/use-cases/logs/fetch-logs/use-case.ts
+++ src/app/logs/application/use-cases/logs/fetch-logs/use-case.ts
@@ -3,6 +3,8 @@ import { UnitOfWork } from "ts-arch-kit/dist/database";
 
 import { UseCase } from "@/app/_common";
 import { NotFoundModelError } from "@/app/_common/errors";
+import { IWorkspaceRepository } from "@/app/organization/application/repos";
+import { Workspace } from "@/app/organization/domain/models/workspace";
 import { IProjectRepository } from "@/app/projects/application/repos";
 import { Project } from "@/app/projects/domain/models/project";
 
@@ -12,20 +14,24 @@ import { FetchLogsUseCaseGateway, FetchLogsUseCaseInput, FetchLogsUseCaseOutput 
 export class FetchLogsUseCase extends UseCase<FetchLogsUseCaseInput, FetchLogsUseCaseOutput> {
     private unitOfWork: UnitOfWork;
     private projectRepository: IProjectRepository;
+    private workspaceRepository: IWorkspaceRepository;
     private logRepository: ILogRepository;
 
     constructor({ repositoryFactory }: FetchLogsUseCaseGateway) {
         super();
         this.unitOfWork = repositoryFactory.createUnitOfWork();
         this.projectRepository = repositoryFactory.createProjectRepository();
+        this.workspaceRepository = repositoryFactory.createWorkspaceRepository();
         this.logRepository = repositoryFactory.createLogRepository();
-        this.unitOfWork.prepare(this.projectRepository, this.logRepository);
+        this.unitOfWork.prepare(this.projectRepository, this.workspaceRepository, this.logRepository);
     }
 
     protected async impl({ projectId, requestUser, queryOptions }: FetchLogsUseCaseInput): Promise<FetchLogsUseCaseOutput> {
         return this.unitOfWork.execute<FetchLogsUseCaseOutput>(async () => {
-            const projectExists = await this.projectRepository.exists({ id: projectId, userId: requestUser.getId() });
-            if (!projectExists) return left(new NotFoundModelError(Project.name, projectId));
+            const project = await this.projectRepository.findOne({ filter: { id: projectId } });
+            if (!project) return left(new NotFoundModelError(Project.name, projectId));
+            const workspace = (await this.workspaceRepository.findById(project.get("workspaceId"))) as Workspace;
+            if (!workspace.userBelongsToWorkspace(requestUser)) return left(new NotFoundModelError(Project.name, projectId));
             const filter = { ...queryOptions?.filter, projectId };
             const count = await this.logRepository.count(filter);
             const logs = await this.logRepository.findSimplified({ ...queryOptions, filter });

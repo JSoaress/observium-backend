64b179fe8a958d54b744aee1fde7b6867ae79050
João Vitor <joao_vitorsgs@hotmail.com>
2025-08-18 19:50:07 -0300

    feat(app/logs): valida se usuario pertence a workspace no use case GetLogById

--- src/app/logs/application/use-cases/logs/get-log-by-id/use-case.ts
+++ src/app/logs/application/use-cases/logs/get-log-by-id/use-case.ts
@@ -4,21 +4,27 @@ import { UnitOfWork } from "ts-arch-kit/dist/database";
 import { UseCase } from "@/app/_common";
 import { MissingParamError, NotFoundModelError } from "@/app/_common/errors";
 import { Log } from "@/app/logs/domain/models/log";
+import { IWorkspaceRepository } from "@/app/organization/application/repos";
+import { Workspace } from "@/app/organization/domain/models/workspace";
+import { IProjectRepository } from "@/app/projects/application/repos";
+import { Project } from "@/app/projects/domain/models/project";
 
-import { IProjectRepository, ILogRepository } from "../../../repos";
+import { ILogRepository } from "../../../repos";
 import { GetLogByIdUseCaseGateway, GetLogByIdUseCaseInput, GetLogByIdUseCaseOutput } from "./types";
 
 export class GetLogByIdUseCase extends UseCase<GetLogByIdUseCaseInput, GetLogByIdUseCaseOutput> {
     private unitOfWork: UnitOfWork;
     private projectRepository: IProjectRepository;
+    private workspaceRepository: IWorkspaceRepository;
     private logRepository: ILogRepository;
 
     constructor({ repositoryFactory }: GetLogByIdUseCaseGateway) {
         super();
         this.unitOfWork = repositoryFactory.createUnitOfWork();
         this.projectRepository = repositoryFactory.createProjectRepository();
+        this.workspaceRepository = repositoryFactory.createWorkspaceRepository();
         this.logRepository = repositoryFactory.createLogRepository();
-        this.unitOfWork.prepare(this.projectRepository, this.logRepository);
+        this.unitOfWork.prepare(this.projectRepository, this.logRepository, this.workspaceRepository);
     }
 
     protected async impl({ id, requestUser }: GetLogByIdUseCaseInput): Promise<GetLogByIdUseCaseOutput> {
@@ -26,8 +32,9 @@ export class GetLogByIdUseCase extends UseCase<GetLogByIdUseCaseInput, GetLogByI
         return this.unitOfWork.execute<GetLogByIdUseCaseOutput>(async () => {
             const log = await this.logRepository.findById(id);
             if (!log) return left(new NotFoundModelError(Log.name, id));
-            const logBelongToUser = await this.projectRepository.exists({ id: log.projectId, userId: requestUser.getId() });
-            if (!logBelongToUser) return left(new NotFoundModelError(Log.name, id));
+            const project = (await this.projectRepository.findOne({ filter: { id: log.get("projectId") } })) as Project;
+            const workspace = (await this.workspaceRepository.findById(project.get("workspaceId"))) as Workspace;
+            if (!workspace.userBelongsToWorkspace(requestUser)) return left(new NotFoundModelError(Log.name, id));
             return right(log);
         });
     }
